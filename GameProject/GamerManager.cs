3fd91428912148c3a6bdcff62fc77b9ae4cac319
BEYZA ARSLAN <2019141005@cumhuriyet.edu.tr>
2021-02-06 23:37:05 +0300

    27 Ocak

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    //Micro Service
+    class GamerManager : IGamerService
+    {
+        IUserValidationService _userValidationService;
+
+        public GamerManager(IUserValidationService _userValidationService)
+        {
+            _userValidationService = _userValidationService;
+        }
+
+        public void  Add(Gamer gamer)
+        {
+            if(_userValidationService.Validate(gamer)==true)
+            {
+                Console.WriteLine("Kayıt oldu");
+            }
+            else
+            {
+                Console.WriteLine("Doğrulama başarısız!!");
+            }
+            Console.WriteLine("Kayıt oldu");
+        }
+
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt silindi");
+
+        }
+
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Kayıt güncellendi");
+
+        }
+    }
+}
